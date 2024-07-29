d5a95c5b2013be223ad0abde82cfbc294f7fe836
AlminMuratspahic89 <131963557+AlminMuratspahic89@users.noreply.github.com>
2024-07-29 11:02:36 +0200

    comm

---
+++ src/main/java/com/TestSlama/TestSlamaApplication.java
@@ -0,0 +1,13 @@
+package com.TestSlama;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestSlamaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestSlamaApplication.class, args);
+	}
+
+}
