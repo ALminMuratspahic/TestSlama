430ed43fe5d94fd23215135eb8f834c88d832571
AlminMuratspahic89 <131963557+AlminMuratspahic89@users.noreply.github.com>
2024-07-29 11:17:14 +0200

    Test c

--- src/main/java/com/TestSlama/TestSlamaApplication.java
+++ src/main/java/com/TestSlama/TestSlamaApplication.java
@@ -1,5 +1,6 @@
 package com.TestSlama;
 
+import java.util.logging.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,7 +9,8 @@ public class TestSlamaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TestSlamaApplication.class, args);
-		System.out.println("Test");
+		Logger logger = Logger.getLogger(TestSlamaApplication.class.getName());
+		logger.info("Info message");
 	}
 
 }
